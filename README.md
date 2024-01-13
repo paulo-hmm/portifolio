62acdcfa2b3efd5999b61dbbaf3a57e8e2317b68
paulo-hmm <phmarquesmartins@gmail.com>
2024-01-13 13:09:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<h1 align="center">DevLinks</h1> 
+
+<p align="center"> 
+Evento exclusivo e gratuito, promovido peça Rocketseat para
+ensino de tecnologias WEB.
+</p>
+
+<p align="center"> 
+    <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+    <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+    <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+    <a href="memo-licença">Licença</a>
+</p>
+
+<p align="center">
+    <img alt="Projeto DevLinks" src="./assets/preview.jpg" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+ - HTML e CSS
+ - JavaScript
+ - Git e Github
+
+## 💻 Projeto
+
+O projeto foi a criação de um portifólio com links.
---
+++ script.js
@@ -0,0 +1,26 @@
+function toogleMode() {
+   const html = document.documentElement
+   let photo = document.querySelector("div#profile")
+/*
+   if (html.classList.contains("light")) {
+    html.classList.remove("light")
+   } else {
+    html.classList.add("light")
+   }
+*/
+   html.classList.toggle("light") // faz o mesmo que a condição acima
+
+   /*Aqui vou coletar a imagem*/
+   const img = document.querySelector("#profile img") //podemos usar igual no CSS
+   
+
+   if (html.classList.contains("light")) {
+    img.setAttribute('src', './assets/fotinha-light.png')
+    img.setAttribute('alt', 'Foto de Paulo Henrique usando camisa preta e fundo rosa')
+   } else {
+    img.setAttribute('src', './assets/fotinha.png')
+    img.setAttribute('alt', 'Foto de Paulo Henrique usando camisa preta e fundo azul')
+
+   }
+
+}
